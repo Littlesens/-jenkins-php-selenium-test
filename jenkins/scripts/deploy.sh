72bccac6e6c518e34047a9bec4e4efa9539ff776
Andersen <xskarry@gmail.com>
2024-07-22 03:42:41 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-
-# Run Docker container with mapped volume and specified port
-docker run -d -p 80:80 --name my-apache-php-app -v /d/jenkins/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
-
-# Give the container some time to start up
+docker run -d -p 80:80 --name my-apache-php-app -v D:\\jenkins\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
-
 set +x
 
 echo 'Now...'
